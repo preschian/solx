40dda8568da7e5210a36d26dc905539c83bc4670
Preschian Febryantara <preschian@users.noreply.github.com>
2025-05-12 11:40:39 +0700

    style: update main.css with new UI color variables and enhance index.vue with a new hero section

--- dapp/app/pages/index.vue
+++ dapp/app/pages/index.vue
@@ -7,4 +7,15 @@
       <UButton>Connect Wallet</UButton>
     </UContainer>
   </div>
+
+  <div class="bg-radial from-neutral-800 via-neutral-700 to-neutral-600">
+    <UContainer class="py-64 text-center max-w-4xl mx-auto">
+      <h1 class="text-6xl font-bold mb-8 text-white">
+        Your Solana Bridge to Web3 Communities and Innovators
+      </h1>
+      <p class="text-2xl text-gray-100 max-w-2xl mx-auto">
+        A comprehensive platform that brings together Web3 initiatives, networks, and digital creators, centralizing all your essential resources in one place
+      </p>
+    </UContainer>
+  </div>
 </template>
